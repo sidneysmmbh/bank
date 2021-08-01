0a1fa8c2950aae3f00f73af9eee39fe3232f8d86
Sidney M. M <sidneysmmbh@hotmail.com>
2021-07-31 22:07:36 -0300

    Modifiquei a classe Conta e CriarConta. E criei a classe Cliente.

--- src/smbank/Conta.java
+++ src/smbank/Conta.java
@@ -1,44 +1,82 @@
 package smbank;
 
 public class Conta {
-	
+
 	private double saldo;
 	private int agencia;
 	private int numero;
-	private String titular;
-	
-	
-	public void depositar (double valor) {
-		this.saldo += valor;
-	}
-	public void sacar (double valor) {
-		if(this.saldo>=valor) {
-			 this.saldo -= valor;
-		}		        
-	}
-	public int getAgencia() {
-		return agencia;
-	}
-	public void setAgencia(int agencia) {
+	private Cliente titular;
+	private static int total = 0;
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("O total de contas é " + Conta.total);
 		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = 100;
+		System.out.println("Estou criando uma conta" + this.numero);
 	}
-	public int getNumero() {
-		return numero;
+
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;
 	}
+
+	public boolean sacar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+
 	public double getSaldo() {
-		return saldo;
+		return this.saldo;
 	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
 	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
 		this.numero = numero;
 	}
-	public String getTitular() {
-		return titular;
+
+	public int getAgencia() {
+		return this.agencia;
 	}
-	public void setTitular(String titular) {
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
-	
-	
-	
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
 
 }
--- src/smbank/CriarConta.java
+++ src/smbank/CriarConta.java
@@ -6,26 +6,29 @@ public class CriarConta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Conta usuario = new Conta();
+		Conta usuario = new Conta(123,4582);
+		Cliente cliente = new Cliente("Carlos Monteiro","568.586-41", "Professor");
 		
-		usuario.setAgencia(148);
-		usuario.setNumero(562);
-		usuario.setTitular("Fernando");
-		usuario.depositar(500);
+		usuario.setTitular(cliente);
+		
+		usuario.deposita(450);
 		
 		System.out.println("Agência:" + usuario.getAgencia()+ " Conta:"
-		                  + usuario.getNumero()+" Titular:"+ usuario.getTitular()
-		                  +" Saldo:" +usuario.getSaldo() );
+                + usuario.getNumero()+" Titular:"+ usuario.getTitular().getNome()
+                +" Saldo:" +usuario.getSaldo() );
+
 		
 		usuario.sacar(200);
 		System.out.println("Agência:" + usuario.getAgencia()+ " Conta:"
-                + usuario.getNumero()+" Titular:"+ usuario.getTitular()
+                + usuario.getNumero()+" Titular:"+ usuario.getTitular().getNome()
                 +" Saldo:" +usuario.getSaldo() );
+
 		
 		usuario.sacar(400);
 		System.out.println("Agência:" + usuario.getAgencia()+ " Conta:"
-                + usuario.getNumero()+" Titular:"+ usuario.getTitular()
+                + usuario.getNumero()+" Titular:"+ usuario.getTitular().getNome()
                 +" Saldo:" +usuario.getSaldo() );
+
 		
 		
 		
